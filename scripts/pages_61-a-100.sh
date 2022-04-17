0bd25d896f22a7e785d18d5595fbfda5c7b7689e
Jean-Marie CODOL <jean-marie.codol@subteq.io>
2022-04-18 00:31:11 +0200

    Update pages_61-a-100.sh

--- scripts/pages_61-a-100.sh
+++ scripts/pages_61-a-100.sh
@@ -11,17 +11,25 @@ DE10NA_ADDR=192.168.1.38
 ###########
 # compiler u-boot
 ssh MY_USER@51.210.241.245
+
 export DEWD=$PWD/de10nano-wd
 mkdir -p $DEWD
 cd $DEWD
 git clone https://github.com/jmecodol/captronic_formation_fpga_img_proc.git
 
 git clone https://github.com/u-boot/u-boot.git
-cd $DEWD/u-boot
-
-
-git clone https://github.com/u-boot/u-boot.git
-cd $DEWD/u-boot
+cd u-boot
 git checkout v2021.07
+
+cd include/
+## less config_distro_bootcmd.h
+patch < $DEWD/captronic_formation_fpga_img_proc/patches/config_distro_bootcmd.patch 
+
+###########
+# page 62 #
+###########
+
+
+
 
 
